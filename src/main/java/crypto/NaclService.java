40320df98b2c2095b751e5b6e027e050ee865aa5
aninath <aninath@cisco.com>
2018-12-19 20:37:11 +0530

    Nacl Box Update ..

--- src/main/java/crypto/NaclService.java
+++ src/main/java/crypto/NaclService.java
@@ -200,6 +200,157 @@ public class NaclService {
 	}
 	
 	@POST
+	@Path("/box/decrypt")
+	@Produces({ "application/json" })
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Response boxdecrypt(@FormParam("p_msg") String message, @FormParam("p_key") String privatekey,@FormParam("p_pubkey") String publickey,@FormParam("p_nonce") String nonce) 
+	{
+		
+		if(null==message || message.trim().length()==0)
+		{
+			return Response.status(Response.Status.NOT_FOUND)
+					.entity(String.format("Message is  EMpty %s", message)).build();
+		}
+		
+		if(null==privatekey || privatekey.trim().length()==0)
+		{
+			return Response.status(Response.Status.NOT_FOUND)
+					.entity(String.format("Private Key is  EMpty %s", privatekey)).build();
+		}
+		
+		if(null==publickey || publickey.trim().length()==0)
+		{
+			return Response.status(Response.Status.NOT_FOUND)
+					.entity(String.format("publickey Key is  EMpty %s", privatekey)).build();
+		}
+		
+		if(publickey.length()!=64)
+		{
+			return Response.status(Response.Status.NOT_FOUND)
+					.entity(String.format("publickey Key is  Invalid must of of 32 bit hex(64) %s", privatekey)).build();
+		}
+		
+		if(privatekey.length()!=64)
+		{
+			return Response.status(Response.Status.NOT_FOUND)
+					.entity(String.format("privatekey Key is  Invalid must of of 32 bit hex(64) %s", privatekey)).build();
+		}
+		
+		
+		byte [] iv;
+		
+		if(null==nonce || nonce.trim().length()==0)
+		{
+		
+			iv = Utils.getIV(24);
+		}
+		
+		if(nonce!=null && nonce.trim().length()<48)
+		{
+			return Response.status(Response.Status.NOT_FOUND)
+					.entity(String.format("Nonce is  Invalid %s it must be 24 bit in Hex(48)", nonce)).build();
+		}
+		else {
+			iv = Utils.hexToBytes(nonce);
+			
+		}
+		nacl nacl = new nacl();
+		String encryptedMesage;
+		try {
+			encryptedMesage = nacl.boxdecrypt(message, publickey, privatekey, iv);
+		} catch (Exception e) {
+			return Response.status(Response.Status.NOT_FOUND)
+					.entity(String.format("Error Message %s ", e)).build();
+		}
+		
+		return Response.status(200).entity(encryptedMesage).build();
+		
+	}
+	
+	
+	@POST
+	@Path("/box/encrypt")
+	@Produces({ "application/json" })
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Response boxencrypt(@FormParam("p_msg") String message, @FormParam("p_key") String privatekey,@FormParam("p_pubkey") String publickey,@FormParam("p_nonce") String nonce) 
+	{
+		
+		if(null==message || message.trim().length()==0)
+		{
+			return Response.status(Response.Status.NOT_FOUND)
+					.entity(String.format("Message is  EMpty %s", message)).build();
+		}
+		
+		if(null==privatekey || privatekey.trim().length()==0)
+		{
+			return Response.status(Response.Status.NOT_FOUND)
+					.entity(String.format("Private Key is  EMpty %s", privatekey)).build();
+		}
+		
+		if(null==publickey || publickey.trim().length()==0)
+		{
+			return Response.status(Response.Status.NOT_FOUND)
+					.entity(String.format("publickey Key is  EMpty %s", privatekey)).build();
+		}
+		
+		if(publickey.length()!=64)
+		{
+			return Response.status(Response.Status.NOT_FOUND)
+					.entity(String.format("publickey Key is  Invalid must of of 32 bit hex(64) %s", privatekey)).build();
+		}
+		
+		if(privatekey.length()!=64)
+		{
+			return Response.status(Response.Status.NOT_FOUND)
+					.entity(String.format("privatekey Key is  Invalid must of of 32 bit hex(64) %s", privatekey)).build();
+		}
+		
+//		if(null==key || key.trim().length()==0)
+//		{
+//			return Response.status(Response.Status.NOT_FOUND)
+//					.entity(String.format("key is  EMpty %s", key)).build();
+//		}
+		
+//		if(key!=null && key.length() !=32 )
+//		{
+//			return Response.status(Response.Status.NOT_FOUND)
+//					.entity(String.format("key length should be 32 %s", key)).build();
+//		}
+		
+		
+		byte [] iv;
+		
+		if(null==nonce || nonce.trim().length()==0)
+		{
+		
+			iv = Utils.getIV(24);
+		}
+		
+		if(nonce!=null && nonce.trim().length()<48)
+		{
+			return Response.status(Response.Status.NOT_FOUND)
+					.entity(String.format("Nonce is  Invalid %s it must be 24 bit in Hex ", nonce)).build();
+		}
+		else {
+			iv = Utils.hexToBytes(nonce);
+			
+		}
+		nacl nacl = new nacl();
+		
+		System.out.println();
+		
+		String encryptedMesage;
+		try {
+			encryptedMesage = nacl.boxencrypt(message, publickey, privatekey, iv);
+		} catch (Exception e) {
+			return Response.status(Response.Status.NOT_FOUND)
+					.entity(String.format("Error Message %s ", e)).build();
+		}
+		return Response.status(200).entity(encryptedMesage).build();
+		
+	}
+	
+	@POST
 	@Path("/decrypt/aead")
 	@Produces({ "application/json" })
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
--- src/main/java/nacl/nacl.java
+++ src/main/java/nacl/nacl.java
@@ -2,35 +2,62 @@ package nacl;
 
 import org.abstractj.kalium.crypto.Advanced;
 import org.abstractj.kalium.crypto.Aead;
+import org.abstractj.kalium.crypto.Box;
 import org.abstractj.kalium.crypto.Random;
+import org.abstractj.kalium.encoders.Hex;
+import org.abstractj.kalium.keys.KeyPair;
+import org.abstractj.kalium.keys.PrivateKey;
+import org.abstractj.kalium.keys.PublicKey;
 
 import cacerts.Utils;
 
 public class nacl {
-	
-	
-	public String aeadencrypt(String plaintext, String aad, byte[] nonce, byte[] key) throws Exception
-	{
+
+	public String boxencrypt(String plaintext,String publickey, String privatekey, byte[] nonce ) throws Exception {
 		try {
-			Aead advanced = new Aead(key);
-			byte[] ciphertext = advanced.encrypt(nonce,plaintext.getBytes(), aad.getBytes()); // encrypt
-			return Utils.toHexEncoded(ciphertext);
+		Box box = new Box(new PublicKey(publickey), new PrivateKey(privatekey));
+		byte[] result = box.encrypt(nonce, plaintext.getBytes());
+		return Utils.toHexEncoded(result);
 		} catch (Exception e) {
 			throw e;
 		}
 	}
 	
-	public String aeaddecrypt(String ciphertext,String aad, byte[] nonce, byte[] key)
-	{
-		byte[]ct = Utils.hexToBytes(ciphertext);
-		Aead advanced = new Aead(key);
-		byte [] plaintext = advanced.decrypt(nonce,ct, aad.getBytes()); // decrypt
-		return new String(plaintext);
-		
-	}
 	
-	public String encrypt(String plaintext, byte[] nonce, byte[] key) throws Exception
-	{
+	public String boxdecrypt(String ciphertext,String publickey, String privatekey, byte[] nonce ) throws Exception {
+		try {
+		byte[] ct = Utils.hexToBytes(ciphertext);
+		Box box = new Box(new PublicKey(publickey), new PrivateKey(privatekey));
+		byte[] result = box.decrypt(nonce, ct);
+		return new String(result);
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+
+	public String aeadencrypt(String plaintext, String aad, byte[] nonce, byte[] key) throws Exception {
+		try {
+			Aead advanced = new Aead(key);
+			byte[] ciphertext = advanced.encrypt(nonce, plaintext.getBytes(), aad.getBytes()); // encrypt
+			return Utils.toHexEncoded(ciphertext);
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+
+	public String aeaddecrypt(String ciphertext, String aad, byte[] nonce, byte[] key) throws Exception {
+		try {
+			byte[] ct = Utils.hexToBytes(ciphertext);
+			Aead advanced = new Aead(key);
+			byte[] plaintext = advanced.decrypt(nonce, ct, aad.getBytes()); // decrypt
+			return new String(plaintext);
+		} catch (Exception e) {
+			throw e;
+		}
+
+	}
+
+	public String encrypt(String plaintext, byte[] nonce, byte[] key) throws Exception {
 		try {
 			Advanced advanced = new Advanced();
 			byte[] ciphertext = advanced.crypto_stream_xsalsa20_xor(plaintext.getBytes(), nonce, key); // encrypt
@@ -39,60 +66,78 @@ public class nacl {
 			throw e;
 		}
 	}
-	
-	public String decrypt(String ciphertext, byte[] nonce, byte[] key)
-	{
-		byte[]ct = Utils.hexToBytes(ciphertext);
+
+	public String decrypt(String ciphertext, byte[] nonce, byte[] key) {
+		byte[] ct = Utils.hexToBytes(ciphertext);
 		Advanced advanced = new Advanced();
-		byte [] plaintext = advanced.crypto_stream_xsalsa20_xor(ct, nonce, key); // decrypt
+		byte[] plaintext = advanced.crypto_stream_xsalsa20_xor(ct, nonce, key); // decrypt
 		return new String(plaintext);
-		
+
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		Random random = new Random();
-        
-        byte[] nonce = random.randomBytes(24);
-        byte[] key = random.randomBytes(32);
-        String pwd = "Hello 8gwifi.org";
-        byte[] plaintext = pwd.getBytes();
-        
-        
-        
-        System.out.println(new String(plaintext));
-        
-       byte[] b =  Utils.getIV(24);
-       
-       String h = Utils.toHexEncoded(b);
-       
-       System.out.println(h);
-       
-       nacl nacl = new nacl();
-       
-       String enc = nacl.encrypt(pwd, nonce, "thisismystrongpasswordof32bitkey".getBytes());
-       System.out.println(enc);
-       String dec = nacl.decrypt(enc, nonce, "thisismystrongpasswordof32bitkey".getBytes());
-       
-       System.out.println(dec);
-       
-       
-       for (int i = 1; i < 40; i++) {
-    	   
-    	   try {
-    		   
-    		   nonce = random.randomBytes(i);
-			enc = nacl.aeadencrypt(pwd, "aead",nonce, "thisismystrongpasswordof32bitkey".getBytes());
-			   System.out.println(enc + "i== " + i);
-		} catch (Exception e) {
-			 
+
+		byte[] nonce = random.randomBytes(24);
+		byte[] key = random.randomBytes(32);
+		String pwd = "Hello 8gwifi.org";
+		byte[] plaintext = pwd.getBytes();
+
+		System.out.println(new String(plaintext));
+
+		byte[] b = Utils.getIV(24);
+
+		String h = Utils.toHexEncoded(b);
+
+		System.out.println(h);
+
+		nacl nacl = new nacl();
+
+		String enc = nacl.encrypt(pwd, nonce, "thisismystrongpasswordof32bitkey".getBytes());
+		System.out.println(enc);
+		String dec = nacl.decrypt(enc, nonce, "thisismystrongpasswordof32bitkey".getBytes());
+
+		System.out.println(dec);
+
+		for (int i = 1; i < 40; i++) {
+
+			try {
+
+				nonce = random.randomBytes(i);
+				enc = nacl.aeadencrypt(pwd, "aead", nonce, "thisismystrongpasswordof32bitkey".getBytes());
+				System.out.println(enc + "i== " + i);
+			} catch (Exception e) {
+
+			}
+			
+			
 		}
-    	   
 		
-	}
-       
-      
-       
-        
+
+		for (int i = 1; i < 50; i++) {
+			try {
+				String ALICE_PUBLIC_KEY  = Utils.toHexEncoded(Utils.getIV(32));
+				String BOB_PRIVATE_KEY   = Utils.toHexEncoded(Utils.getIV(32));
+				
+				nonce = random.randomBytes(i);
+				
+				 enc =nacl.boxencrypt(pwd, ALICE_PUBLIC_KEY, BOB_PRIVATE_KEY, nonce);
+				 
+				 System.out.println(enc);
+				 
+				 dec = nacl.boxdecrypt(enc, ALICE_PUBLIC_KEY, BOB_PRIVATE_KEY, nonce);
+				 
+				 System.out.println("Box Decrypted"+  dec + "i ==" + i);
+				 
+				
+			} catch (Exception e) {
+				//e.printStackTrace();
+				
+			}
+		}
+		
+		
+
 	}
 
 }
